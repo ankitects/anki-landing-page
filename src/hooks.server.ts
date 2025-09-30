308c20f053062234e20406445019a58f406f79db
ittipat <39715559+ittipatken@users.noreply.github.com>
2025-09-30 17:46:31 +0700

    fix: server hook

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,16 +1,20 @@
-import { sequence } from '@sveltejs/kit/hooks';
-import { type Handle } from '@sveltejs/kit';
 import { generateBundles, negotiateLocale } from '$lib/fluent';
+import type { Handle } from '@sveltejs/kit';
 import { createSvelteFluent } from '@nubolab-ffwd/svelte-fluent';
+import { sequence } from '@sveltejs/kit/hooks';
 
-const preloadFonts: Handle = async ({ event, resolve }) => {
-  const response = await resolve(event, {
-    preload: ({ type }) => type === 'font',
-  });
+const fluentHandler: Handle = async ({ event, resolve }) => {
   event.locals.locale = negotiateLocale(event);
   event.locals.fluent = createSvelteFluent(generateBundles(event.locals.locale));
 
+  return resolve(event);
+};
+
+const preloadFonts: Handle = async ({ event, resolve }) => {
+  const response = await resolve(event, {
+    preload: ({ type }) => type === 'font'
+  });
   return response;
 };
 
-export const handle: Handle = sequence(preloadFonts);
+export const handle: Handle = sequence(fluentHandler, preloadFonts);
